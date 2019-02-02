9f59eeecea33d6934fe0d109eb4451697e62cb56
赵鹏程 <zhaopengcheng02@meituan.com>
2019-02-02 10:56:52 +0800

    第一次提交

--- src/index.js
+++ src/index.js
@@ -2,11 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import icon16 from 'images/icon16';
+import icon48 from 'images/icon48';
+import icon128 from 'images/icon128';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
